7ed5df564d7806f7b022b365c1ea85c424beecd8
skgch <m.sakaguchi04@gmail.com>
2018-08-04 18:08:33 +0900

    first commit

---
+++ src/components/GoogleMap.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'google-map',
+
+  mounted () {
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: { lat: 61.180059, lng: -149.822075 },
+      zoom: 14
+    })
+  }
+}
+</script>
+
+<style scoped>
+  #map {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    background: gray;
+  }
+</style>
---
+++ static/init.js
@@ -0,0 +1,2 @@
+const api = document.getElementById('gmaps-api')
+api.setAttribute('src', `https://maps.googleapis.com/maps/api/js?key=${APIKEY}`)
